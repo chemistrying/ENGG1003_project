5dbae554eab3af8ae3e166aee5927706c332a1c2
sy10080 <sy10080@syss.edu.hk>
2023-11-15 23:29:52 +0800

    Add data_fetch.py (for batch fetching data)

---
+++ data_fetch.py
@@ -0,0 +1,26 @@
+from airport import *
+from datetime import date, timedelta
+import asyncio
+import json
+
+async def main(loop):
+    today = date.today()
+    client = Fetcher(loop, 'dynamic')
+    for i in range(1, 92):
+        require_date = today - timedelta(i)
+        
+        arrival_data = await client.fetch_arrival(require_date)
+        with open(f"arrival\\{require_date.strftime('%Y-%m-%d')}.json", "w") as f:
+            json.dump(arrival_data, f, indent=4, cls=CustomEncoder)
+        
+        departure_data = await client.fetch_departure(require_date)
+        with open(f"departure\\{require_date.strftime('%Y-%m-%d')}.json", "w") as f:
+            json.dump(departure_data, f, indent=4, cls=CustomEncoder)
+
+
+loop = asyncio.get_event_loop()
+main_task = loop.create_task(main(loop))
+loop.run_until_complete(main_task)
+
+
+    
